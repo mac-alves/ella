6319036e2b583d2102da34691bbd43dbc363abbf
mac-alves <mauricio.bleck7@gmail.com>
2020-09-23 22:30:07 -0300

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:ella/screens/home_screen.dart';
+import 'package:ella/utils/constants.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        brightness: Brightness.light,
+        scaffoldBackgroundColor: Colors.white,
+        textTheme:
+            GoogleFonts.poppinsTextTheme().apply(displayColor: kTextColor),
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
